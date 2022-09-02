fix: remove expiration as it conflicts with gradient

quantizer_pytorch/quantizer.py
@@ -295,7 +295,7 @@ def l2norm(x: Tensor) -> Tensor:
 
 
 class HVQ(Quantization):
-    """Hybrid vector quantization: multiheaded, cosine sim, gumbel, expiration"""
+    """Hybrid vector quantization: multiheaded, cosine sim, gumbel"""
 
     def __init__(
         self,
@@ -303,7 +303,6 @@ class HVQ(Quantization):
         num_heads: int,
         codebook_size: int,
         temperature: float = 0.5,
-        expire_threshold: int = 0,
         ema_decay: float = 0.99,
     ):
         super().__init__()
@@ -314,19 +313,11 @@ class HVQ(Quantization):
         self.num_heads = num_heads
         self.head_features = features // num_heads
         self.codebook_size = codebook_size
-        self.expire_threshold = expire_threshold
-        self.ema_decay = ema_decay
 
         # Initialize codebook (h, m, d)
         codebooks = torch.randn(num_heads, codebook_size, self.head_features)
         self.codebooks = nn.Parameter(codebooks)
         nn.init.kaiming_uniform_(self.codebooks)
-
-        # Track codebook cluster size to expire dead codes faster
-        ema_cluster_size = torch.full(
-            size=(num_heads, codebook_size), fill_value=float(self.expire_threshold)
-        )
-        self.register_buffer("ema_cluster_size", ema_cluster_size)
 
     def from_ids(self, indices: LongTensor) -> Tensor:
         b = indices.shape[0]
@@ -360,60 +351,8 @@ class HVQ(Quantization):
         out = einsum("b h n i, b h j d -> b h n d", attn, c)
         out = rearrange(out, "b h n d -> b n (h d)")
 
-        info = {
-            "indices": codebook_indices,
-            "perplexity": perplexity(attn),
-            "replaced_codes": self.track_and_expire_dead_codes(x, onehot=attn),
-        }
+        info = {"indices": codebook_indices, "perplexity": perplexity(attn)}
         return out, info
-
-    def track_and_expire_dead_codes(
-        self, x: Tensor, onehot: Tensor
-    ) -> Optional[Tensor]:
-        """Replaces dead codes in codebook with random batch elements"""
-
-        if self.expire_threshold <= 0:
-            return None
-
-        if self.training:
-            # Update codebook cluster sizes with EMA
-            batch_cluster_size = reduce(onehot, "b h n m -> b h m", "sum")
-            avg_cluster_size = reduce(batch_cluster_size, "b h m -> h m", "mean")
-            ema_inplace(self.ema_cluster_size, avg_cluster_size, self.ema_decay)
-
-        # Mask is true where codes are expired
-        expired_codes_per_head = self.ema_cluster_size < self.expire_threshold  # type: ignore # noqa
-        num_expired_codes_per_head = reduce(expired_codes_per_head, "h m -> h", "sum")
-
-        # Return if no heads with expired codes, or if not training
-        if torch.all(num_expired_codes_per_head == 0) or not self.training:
-            return num_expired_codes_per_head
-
-        # Candidate vectors for codebook replacement
-        vectors = rearrange(x, "b h d -> (b h) d")
-        n, device = vectors.shape[0], x.device
-        new_codebooks = self.codebooks.data
-
-        for head_idx in range(self.num_heads):
-            num_expired_codes = num_expired_codes_per_head[head_idx]
-            expired_codes = expired_codes_per_head[head_idx]  # type: ignore
-
-            if n < num_expired_codes:
-                # If fewer new samples than expired codes, repeat random duplicates
-                ids = torch.randint(0, n, (num_expired_codes,), device=device)
-            else:
-                # If more new samples than expired codes, pick random candidates
-                ids = torch.randperm(n, device=device)[0:num_expired_codes]
-
-            # Update codebook head
-            h_start = head_idx * self.head_features
-            h_end = h_start + self.head_features
-            new_codebooks[head_idx, expired_codes] = vectors[ids, h_start:h_end]
-
-        with torch.no_grad():
-            self.codebooks.copy_(new_codebooks)
-
-        return num_expired_codes_per_head
 
 
 class ResidualHVQ(Quantization):
@@ -447,7 +386,7 @@ class ResidualHVQ(Quantization):
         assert r <= self.num_residuals, "num_residuals must be <= number of residuals"
 
         out, residual = torch.zeros_like(x), x
-        all_indices, all_perplexities, all_replaced_codes = [], [], []
+        all_indices, all_perplexities = [], []
 
         for i in range(r):
             quantized, info = self.quantizers[i](residual)
@@ -455,14 +394,10 @@ class ResidualHVQ(Quantization):
             out = out + quantized
             all_indices += [info["indices"]]
             all_perplexities += [info["perplexity"]]
-            all_replaced_codes += [info["replaced_codes"]]
 
         info = {
             "indices": rearrange(all_indices, "r b h n -> b h (n r)"),
             "perplexity": rearrange(all_perplexities, "r h -> (h r)"),
-            "replaced_codes": rearrange(all_replaced_codes, "r h -> (h r)")
-            if exists(all_replaced_codes[0])
-            else None,
         }
 
         return out, info
@@ -547,7 +482,6 @@ class Quantizer1d(nn.Module):
         g, s = self.num_groups, t // self.split_size
         # Quantize each group in a different head (codebook)
         x = rearrange(x, "b (g k) (s d) -> b (k s) (g d)", g=g, s=s)
-        print(x.shape)
         x, info = self.quantize(x)
         # Mask channel tokens with increasing probability
         tokens = rearrange(x, "b (k s) (g d) -> (b s) (g k) d", g=g, s=s)